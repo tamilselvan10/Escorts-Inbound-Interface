636604c19f9d3861f22cbd098db102d985065688
tamilselvan10 <111409280+tamilselvan10@users.noreply.github.com>
2022-09-13 17:39:58 +0530

    Add files via upload

---
+++ Tables/xxes_in_process_conn_log_tb.sql
@@ -0,0 +1,13 @@
+Create table xxes_in_process_conn_log_tb
+(
+id                NUMBER,
+Interface_name    VARCHAR2(4000),
+source            VARCHAR2(4000),
+destination       VARCHAR2(4000),
+connection_status VARCHAR2(4000),
+error_message     VARCHAR2(4000),
+connection_check_date DATE
+);
+
+GRANT ALL ON xxes_in_process_conn_log_tb TO APPS;
+CREATE OR REPLACE SYNONYM xxes_in_process_conn_log_tb FOR xxes.xxes_in_process_conn_log_tb;
